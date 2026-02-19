8792e28f1b787a7c438a54826b97aa1ce4a2d9a2
Ikerby <novoliva@gmail.com>
2026-02-19 17:57:53 +0100

    initial commit

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);	// Ruta d'aquest arxiu (servidor.js)
+const __dirname = path.dirname(__filename);			// Ruta de la carpeta on es troba aquest arxiu
+
+const app = express();
+const PORT = 3000;
+
+// Middleware per convetir JSON
+app.use(express.json());
+
+// Servir arxius estàtics des de la carpeta 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Carpeta on es troben les plantilles (arxius .ejs)
+//	i el motor que s'utilitzarà per generar les pàgines html
+app.set('views', path.join(__dirname, '../plantilles'));
+app.set('view engine', 'ejs');
+
+// Ruta d'inici (http://localhost:3000)
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
