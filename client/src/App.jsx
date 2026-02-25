476dd08ce6fe30f7cbfb05b866b64c3916aa10b9
Ikerby <novoliva@gmail.com>
2026-02-25 20:21:09 +0100

    Primeres pases amb React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './styles/App.css'
+import logo from './assets/logo.png'
+import userIcon from './assets/usuari.png'
+import favoriteIcon from './assets/favorito.png'
+import directoryIcon from './assets/directorio.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
     </>
   )
 }
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbarDiv">
+        <img src={logo} alt="Logo" className="logo" />
+        <input type="text" placeholder="Buscar anime..." className="searchBar" />
+      </div>
+      <div className="navbarDiv">
+        < ButtonNavBar img={directoryIcon} />
+        < ButtonNavBar img={favoriteIcon} />
+        < ButtonNavBar img={userIcon} paddingLeft="0.5rem" />
+      </div>
+    </nav>
+  )
+}
+
+function ButtonNavBar({ img, paddingLeft }) {
+  return (
+      <button className="buttonNavBar" style={{ paddingLeft: paddingLeft }}>
+        <img src={img} alt="Icon" className="icon" />
+      </button>
+  )
+}
 export default App
