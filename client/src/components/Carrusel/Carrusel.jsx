8c8f89c76faa1d16b378d7ca2a5104df7e2ccccf
Ikerby <novoliva@gmail.com>
2026-04-17 17:08:32 +0200

    Centrada la flecha izquierda del carrusel

--- client/src/components/Carrusel/Carrusel.jsx
+++ client/src/components/Carrusel/Carrusel.jsx
@@ -33,8 +33,8 @@ export function Carrusel({ items = [], images = [], onItemClick }) {
   return (
     <div className="coverflow-carousel">
       <button className="coverflow-arrow coverflow-arrow--left" onClick={handlePrevious} aria-label="Anterior">
-        <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 96 960 960" width="28" fill="currentColor">
-          <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+        <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 96 960 960" width="28" fill="currentColor" style={{ transform: 'scaleX(-1)' }}>
+          <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
         </svg>
       </button>
 
